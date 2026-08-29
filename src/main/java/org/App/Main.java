c14fd7f30829b5f7b291a952a881f08e5c3c9618
piales00 <perez.izquierdo.piero@gmail.com>
2026-08-28 21:34:42 -0500

    feat: add products

--- src/main/java/org/App/Main.java
+++ src/main/java/org/App/Main.java
@@ -1,7 +1,27 @@
 package org.App;
 
+import org.Catalogo.Producto;
+import org.Catalogo.Catalogo;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hola mundo, este es el inicio del proyecto");
+    System.out.println("products testing");
+    System.out.println("Creando producto");
+    Catalogo catalogo = new Catalogo(10);
+    Producto producto2 = new Producto("Filtro");
+    Producto producto1 = new Producto("Balsa");
+
+    System.out.println("Agregando producto");
+    catalogo.agregarProducto(producto2);
+    catalogo.agregarProducto(producto1);
+    catalogo.mostrarProductos();
+
+    Producto productoSearch = catalogo.buscarProducto("Filtro");
+
+    System.out.println("Busqueda de producto: " + productoSearch);
+
+    System.out.println("ELIMINAR PRODUCTO");
+    catalogo.eliminarProducto("Balsa");
+    catalogo.mostrarProductos();
   }
 }
